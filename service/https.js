7d0c8ce40e858caedda25ce70622df92f9b27ebe
lianpen <767034650@qq.com>
2018-07-27 11:19:37 +0800

    .

--- service/https.js
+++ service/https.js
@@ -6,21 +6,15 @@
 const app = require('./app')
 var fs = require('fs');
 const args = require('./args')
-const port = args.port || 80
+const port = args.port || 443
 
-/*
 var https = require('https');
 var options = {
-    key:fs.readFileSync('./https/production/214744561240215.key'),
-    cert:fs.readFileSync('./https/production/214744561240215.pem')
+	key: fs.readFileSync('./https/1532902641737.key'),
+	cert: fs.readFileSync('./https/1532902641737.pem')
 }
-console.log(options)
 var httpsServer = https.createServer(options, app)
 
 httpsServer.listen(port, function() {
-	console.log('服务启动在' + port + '端口')
-});
-*/
-app.listen(port, function() {
 	console.log('服务启动在' + port + '端口')
 });
